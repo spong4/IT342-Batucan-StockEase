7c264a21972a4875ebcff72215fafb8dd8d698bd
LAB13_USER ON WS40 <L13Y17W40@LabHCI.net>
2026-03-07 15:48:47 +0800

    Intial Commit

---
+++ StockEase/StockEase/src/main/java/edu/cit/batucan/StockEase/StockEaseApplication.java
@@ -0,0 +1,13 @@
+package edu.cit.batucan.StockEase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockEaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockEaseApplication.class, args);
+	}
+
+}
---
+++ StockEase/StockEase/src/test/java/edu/cit/batucan/StockEase/StockEaseApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.batucan.StockEase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockEaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
