cfc93d848991f1d324aa616d8bc125ed19f16351
LAB13_USER ON WS40 <L13Y17W40@LabHCI.net>
2026-03-28 17:36:18 +0800

    Mobile Integration
    (Still needs to connect to backend)

---
+++ mobile/app/src/main/java/com/it342/stockease/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.it342.stockease
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.*
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.it342.stockease.ui.HomeScreen
+import com.it342.stockease.ui.LoginScreen
+import com.it342.stockease.ui.RegisterScreen
+import androidx.compose.material3.MaterialTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme  {
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = "login"
+                ) {
+                    composable("login") {
+                        LoginScreen(
+                            onLoginSuccess = {
+                                navController.navigate("home") {
+                                    popUpTo("login") { inclusive = true }
+                                }
+                            },
+                            onNavigateToRegister = {
+                                navController.navigate("register")
+                            }
+                        )
+                    }
+                    composable("register") {
+                        RegisterScreen(
+                            onRegisterSuccess = {
+                                navController.navigate("login") {
+                                    popUpTo("register") { inclusive = true }
+                                }
+                            },
+                            onNavigateToLogin = {
+                                navController.popBackStack()
+                            }
+                        )
+                    }
+                    composable("home") {
+                        HomeScreen()
+                    }
+                }
+            }
+        }
+    }
+}
